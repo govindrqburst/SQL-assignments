9883f604fb22c1102e23bc533bf5d512bf550b5b
govindr <govindr@qburst.com>
2016-07-14 18:14:52 +0530

    Added a second line

---
+++ assignment_1.sql
@@ -0,0 +1,5 @@
+create database BankDB;
+create table savingsaccount2;
+insert into table savingsaccount2(account_no int, transaction_time_date datetime, transaction_type varchar(20), transaction_amt int, current_balance);
+
+One more line.
